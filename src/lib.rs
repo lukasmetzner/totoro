30bfb4f5bb9cca26189a6906511856d848f8c4e5
Lukas Metzner <lukas@metzner.io>
2023-12-12 22:09:42 +0100

    builder pattern not necessary

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-pub mod builder;
 pub mod config;
 pub mod message;
 pub mod server;
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use anyhow::Result;
-
-use totoro::builder::TotoroBuilder;
+use totoro::server::TotoroServer;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
-    let server = TotoroBuilder::new().build().await?;
+    let server = TotoroServer::new(Default::default()).await?;
     server.run().await?;
     Ok(())
 }
